7c36cb48bfd6660c2b426c17df3f9cd4d5802bfe
DevDocXIII <dshelfoon@gmail.com>
2025-07-27 16:12:31 -0400

    Just saving

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import pygame
-
+from constants import  (
+    SCREEN_WIDTH, 
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS, 
+    ASTEROID_MAX_RADIUS, 
+    ASTEROID_KINDS, 
+    ASTEROID_SPAWN_RATE
+    )
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
     print("Starting Asteroids!")
+    print(f"Screen width: {1280}") 
+    print(f"Screen height: {720}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
+        # Update and draw game elements here (e.g., asteroids)
+        # For now, just fill the screen with black
+        screen.fill((0, 0, 0))
+        
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
